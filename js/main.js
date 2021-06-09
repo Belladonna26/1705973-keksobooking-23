e22fca2c329ee2156411a183138ef0c193ed0369
Helen <alena.samsa@yandex.ru>
2021-06-09 17:06:08 +0400

    Улучшение внешнего вида функции

--- js/main.js
+++ js/main.js
@@ -1,25 +1,4 @@
-// function getRandomNumber(min, max) {
-//   if(max <= min || min < 0) {
-//     throw new Error('Введено отрицательное число или максимум меньше минимума');
-//   }
-//   return Math.floor(Math.random()  * (max - min + 1) ) + min;
-// }
-
-// getRandomNumber();
-
-// function getRandomFloatNumber(min, max, precision) {
-//   if(max <= min || min < 0) {
-//     throw new Error('Введено отрицательное число или максимум меньше минимума');
-//   }
-//   const randomFloatNumber = Math.random() * (max-min) + min;
-//   return parseFloat(randomFloatNumber.toFixed(precision));
-// }
-
-// getRandomFloatNumber(4,25,4);
-
-
-//module4-task2
-const AUTHOR_AVATAR = [
+const AUTHOR_AVATARS = [
   'img/avatars/user01.png',
   'img/avatars/user02.png',
   'img/avatars/user03.png',
@@ -52,40 +31,33 @@ const ADDRESSES = [
   '71,89',
 ];
 
-const PRICES = [
-  5000,
-  3500,
-  1000,
-  1500,
-  2000,
-  500,
-  4300,
-  1200,
+const MIN_PRICE = 500;
+
+const MAX_PRICE = 5000;
+
+const HousingType = {
+  palace: 'palace',
+  flat: 'flat',
+  house: 'house',
+  bungalow: 'bungalow',
+  hotel: 'hotel',
+};
+
+const HOUSING_TYPES = [
+  HousingType.palace,
+  HousingType.flat,
+  HousingType.house,
+  HousingType.bungalow,
+  HousingType.hotel,
 ];
 
-const TYPE_OF_HOUSING = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow',
-  'hotel',
-];
+const MIN_ROOMS = 1;
 
-const ROOMS = [
-  1,
-  2,
-  3,
-  4,
-  5,
-];
+const MAX_ROOMS = 5;
 
-const GUESTS = [
-  1,
-  2,
-  3,
-  4,
-  5,
-];
+const MIN_GUESTS_COUNT = 1;
+
+const MAX_GUESTS_COUNT = 5;
 
 const CHECKIN = [
   '12:00',
@@ -99,16 +71,25 @@ const CHECKOUT = [
   '14:00',
 ];
 
+const Feature = {
+  wifi: 'wifi',
+  dishwasher: 'dishwasher',
+  parking: 'parking',
+  washer: 'washer',
+  elevator: 'elevator',
+  conditioner: 'conditioner',
+};
+
 const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
+  Feature.wifi,
+  Feature.dishwasher,
+  Feature.parking,
+  Feature.washer,
+  Feature.elevator,
+  Feature.conditioner,
 ];
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'Отличное состояние, имеется все для уютного проживания',
   'Рядом метро и вся нужная транспортная развязка. Чистая квартира',
   'Чистая, уютная комната. Идеально подойдет для студентов',
@@ -122,51 +103,62 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const OBJECT_COUNT = 10;
+const MIN_LATTITUDE = 35.65000;
+
+const MAX_LATTITUDE = 35.70000;
+
+const MIN_LONGTITUDE = 139.70000;
+
+const MAX_LONGTITUDE = 139.80000;
+
+const COORDINATES_PRECISION = 5;
+
+const AD_COUNT = 10;
+
 const getRandomArrayElement = (elements) => elements[_.random(0, elements.length - 1)];
 
-function getRandomFloatNumber(min, max, precision) {
+const getRandomFloatNumber = (min, max, precision) => {
   if(max <= min || min < 0) {
     throw new Error('Введено отрицательное число или максимум меньше минимума');
   }
   const randomFloatNumber = Math.random() * (max-min) + min;
   return parseFloat(randomFloatNumber.toFixed(precision));
-}
+};
 
-function getRandomNumber(min, max) {
+const getRandomNumber = (min, max) => {
   if(max <= min || min < 0) {
     throw new Error('Введено отрицательное число или максимум меньше минимума');
   }
   return Math.floor(Math.random()  * (max - min + 1) ) + min;
-}
+};
 
 const getRandomArray = (array) => {
-  const randomArray = new Array(getRandomNumber(1, array.length)).fill(' ').map(() => (getRandomArrayElement(array)));
+  const randomArray = Array.from({length: getRandomNumber(1, array.length)}).map(() => getRandomArrayElement(array));
   const newRandomElementsArray = [...new Set(randomArray)];
   return newRandomElementsArray;
 };
 
 const createAuthor = () => ({
-  avatar: getRandomArrayElement(AUTHOR_AVATAR),
+  avatar: getRandomArrayElement(AUTHOR_AVATARS),
 });
 
 const createOffer = () => ({
   title: getRandomArrayElement(TITLES),
   address: getRandomArrayElement(ADDRESSES),
-  price: getRandomArrayElement(PRICES),
-  type: getRandomArrayElement(TYPE_OF_HOUSING),
-  rooms: getRandomArrayElement(ROOMS),
-  guests: getRandomArrayElement(GUESTS),
+  price: getRandomNumber(MIN_PRICE, MAX_PRICE),
+  type: getRandomArrayElement(HOUSING_TYPES),
+  rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
+  guests: getRandomNumber(MIN_GUESTS_COUNT, MAX_GUESTS_COUNT),
   checkin: getRandomArrayElement(CHECKIN),
   checkout: getRandomArrayElement(CHECKOUT),
   features: getRandomArray(FEATURES),
-  description: getRandomArrayElement(DESCRIPTION),
+  description: getRandomArrayElement(DESCRIPTIONS),
   photos: getRandomArray(PHOTOS),
 });
 
 const createLocation = () => ({
-  lat: getRandomFloatNumber(35.65000,35.70000,5),
-  lng: getRandomFloatNumber(139.70000,139.80000,5),
+  lat: getRandomFloatNumber(MIN_LATTITUDE, MAX_LATTITUDE, COORDINATES_PRECISION),
+  lng: getRandomFloatNumber(MIN_LONGTITUDE, MAX_LONGTITUDE, COORDINATES_PRECISION),
 });
 
 const createAd = () => {
@@ -178,9 +170,9 @@ const createAd = () => {
   return ad;
 };
 
-const lodging = new Array(OBJECT_COUNT).fill(null).map(() =>
+const ads = Array.from({length: AD_COUNT}).map(() =>
   createAd());
 
 // eslint-disable-next-line no-console
-console.log(lodging);
+console.log(ads);
 
